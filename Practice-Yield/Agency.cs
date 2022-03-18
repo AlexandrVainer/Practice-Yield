c5e2e04520d35b3ac656a72982323d53f2d84eeb
alexva743 <alexva743@gmail.com>
2022-03-18 09:18:47 +0200

    init

---
+++ Practice-Yield/Agency.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Practice_Yield
+{
+    public class Agency
+    {
+        public Car[] Cars { get; set; }
+
+        public IEnumerator<Car> GetEnumerator()
+        {
+            foreach (Car car in Cars)
+                yield return car;
+        }
+        public IEnumerable<Car> GetCars(int modelYear)
+        {
+            foreach (Car car in Cars)
+                if (car.ModelYear == modelYear)
+                {
+                    yield return car;
+                }
+        }
+        public IEnumerable<Car> GetCars(string maker)
+        {
+            foreach (Car car in Cars)
+                if (car.Maker == maker)
+                {
+                    yield return car;
+                }
+        }
+
+    }
+}
---
+++ Practice-Yield/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Yield
+{
+    public class Car
+    {
+        public string ?Maker { get; set; }
+        public int ModelYear { get; set; }
+        public override string ToString()
+        {
+            return $"{ModelYear},{Maker}";
+        }
+
+    }
+}
